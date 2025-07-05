816b4058cfd68b17e3bbbd3b6191a5f817388265
Jeongwoo Park <wahoo9040@naver.com>
2025-07-05 16:10:13 +0900

    feat: TypographyEditor에 뒤로 가기 버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Canvas } from "@/components/Canvas/Canvas";
 import Sidebar from "@/components/SIdebar/Sidebar";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-react";
 
 export default function TypographyEditor() {
   return (
-    <div className="flex h-screen bg-background">
+    <div className="relative flex h-screen bg-background">
+      <Button variant={"outline"} size="icon" className="absolute top-4 left-4 z-9 ">
+        <ArrowLeftIcon />
+      </Button>
       <Canvas />
       <Sidebar />
     </div>
