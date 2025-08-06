e0aa19a82e1cf045f11492ec46a8b5803f88cca4
Jeongwoo Park <wahoo9040@naver.com>
2025-08-06 22:33:58 +0900

    feat: 패키지 이름 확정 및 배포

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
           </div>
           <span className="sm:flex hidden flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-5 -mb-12 max-[800px]:mb-12 font-code sm:text-base text-sm font-medium">
             <TerminalSquareIcon className="w-5 h-5 sm:mr-1 mt-0.5" />
-            {"npm install mesa"}
+            {"npm install mesa-react"}
           </span>
         </div>
 
