23e2f44d3a7687616e0c5cb5871aab719e8071b2
Jeongwoo Park <wahoo9040@naver.com>
2025-08-06 22:54:39 +0900

    chore: 타입 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'Mesa',
-      fileName: 'mesa'
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "Mesa",
+      fileName: "mesa",
     },
     rollupOptions: {
       external: [],
       output: {
-        globals: {}
-      }
-    }
-  }
-})
+        globals: {},
+      },
+    },
+  },
+  plugins: [dts()],
+});
