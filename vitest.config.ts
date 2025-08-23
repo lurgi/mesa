9274012540c0bf05f8887927fddfa6ea4e660763
Jeongwoo Park <wahoo9040@naver.com>
2025-08-23 22:43:15 +0900

    test: Jest에서 Vitest로 테스트 프레임워크 변경

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    include: ["tests/**/*.{ts,tsx}"],
+    globals: true,
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/**/*.d.ts", "src/main.ts", "src/vite-env.d.ts"],
+    },
+  },
+});
